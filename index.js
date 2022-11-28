fe877b66cc7ddabf097d804a6bee168a9432fb28
Manny <krav23@hotmail.com>
2022-11-28 16:33:17 -0500

    Begin react learning

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// Allows us to render HTML to our page using JSX!
+
+//The 1st arg is WHAT u want to render
+//The 2nd arg is WHERE u want to render
+
+ReactDOM.render(
+    <h1>Hello, Everyone</h1>,
+
+    document.getElementById('root')
+);
